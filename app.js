a24f484bb027c7151344ac5b853f1992d93420ac
萧泳森 <xiaoys@ligeit.com>
2021-01-14 16:56:37 +0800

    feat: express 的 hello world! demo

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
+
+app.get('/xxx', (request, response) => {
+  response.send('This is a XXX page!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
